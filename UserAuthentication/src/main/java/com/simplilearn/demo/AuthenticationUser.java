f74589b4a0cf8c5d15adb69d3e0ccfd13234225b
your Name <mjhy2000@gmail.com>
2022-09-03 14:08:28 +0530

    user authentication

---
+++ UserAuthentication/src/main/java/com/simplilearn/demo/AuthenticationUser.java
@@ -0,0 +1,16 @@
+package com.simplilearn.demo;
+
+public class AuthenticationUser {
+	public String username()
+	{
+		String email = "Harish@gmail.com";
+		return email;
+	}
+	
+	public String paswd()
+	{
+		String password = "Tamizhan5432.";
+		return password;
+	}
+
+}
